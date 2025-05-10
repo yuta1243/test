12e03c564e285e3fbf2ba1ccf84c9409a115703a
yuta uchiyama <uchiyamayuta1243@gmail.com>
2025-05-10 21:20:54 +0900

    first_commit

---
+++ test/init.py
@@ -0,0 +1,10 @@
+def calc(a, b):
+    result = a + b
+    return result
+
+print("Hello!")
+
+user_input_1 = int(input("Please enter the 1st number: "))
+user_input_2 = int(input("Please enter the 2nd number: "))
+
+print("The sum result is: ", calc(user_input_1, user_input_2))
